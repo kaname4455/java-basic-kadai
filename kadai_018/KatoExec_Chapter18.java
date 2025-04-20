05a1943a2a95e31ff3fab313b6ab024184a2c7d0
kaname4455 <kanamebaseball2721@gmail.com>
2025-04-20 17:03:33 +0900

    課題を再再アップロード

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	
+	 public static void main(String[] args) {
+		    KatoTaro_Chapter18 Taro  = new KatoTaro_Chapter18();
+		    Taro.execIntroduce();
+		    System.out.println();
+		    
+		    KatoIchiro_Chapter18 Ichiro  = new KatoIchiro_Chapter18();
+		    Ichiro.execIntroduce();
+		    System.out.println();
+		    
+		    KatoHanako_Chapter18 Hanako  = new KatoHanako_Chapter18();
+		    Hanako.execIntroduce();
+		    System.out.println();
+		    
+		 }
+
+}
